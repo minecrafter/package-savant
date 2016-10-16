2d6561a2297cda8691ef3196f34c1808b6bdfe6e
Tux <write@imaginarycode.com>
2016-10-16 13:25:18 -0400

    A little more work on the UI

--- util/http_responses.go
+++ util/http_responses.go
@@ -10,18 +10,17 @@ import (
 )
 
 var (
-	fourOhFour = bindata.MustAsset("templates/404.html")
-	base       *template.Template // populated later
+	base *template.Template // populated later
 )
 
 func init() {
 	files := []string{"templates/footer.html", "templates/header.html", "templates/internal_error.html",
-		"templates/main.html"}
-	var buf bytes.Buffer
+		"templates/main.html", "templates/404.html"}
+	var concatenatedTemplate bytes.Buffer
 	for _, file := range files {
-		buf.Write(bindata.MustAsset(file))
+		concatenatedTemplate.Write(bindata.MustAsset(file))
 	}
-	base = template.Must(template.New("base").Parse(buf.String()))
+	base = template.Must(template.New("base").Parse(concatenatedTemplate.String()))
 }
 
 func mustTemplateAsset(name string) *template.Template {
@@ -32,7 +31,7 @@ func mustTemplateAsset(name string) *template.Template {
 func Do404(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusNotFound)
-	w.Write(fourOhFour)
+	base.ExecuteTemplate(w, "four_oh_four", nil)
 }
 
 func DoGenericError(w http.ResponseWriter) {
